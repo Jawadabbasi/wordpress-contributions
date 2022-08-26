d365d2d38996b374c396ed2580669735b3f04dd4
Jawadabbasi <Jawadabbasi42@gmail.com>
2022-08-26 23:15:55 +0500

    Add files via upload

---
+++ get_footer/README.md
@@ -0,0 +1,4 @@
+Create footer-new.php file in theme main directory and put that code <br>
+<b>Output:</b><br>
+Muhammad Jawad Abbasi <br>
+Pakistan
---
+++ get_footer/footer-new.php
@@ -0,0 +1,6 @@
+<?php echo $args['name']; ?> <br> <?php echo $args['location']; ?>
+// that code put in page.php, single.php etc
+// I have put that code in index.php file
+<?php
+get_footer('new',array('name'=>'Muhammad Jawad Abbasi','location'=>'Pakistan'));
+?>
