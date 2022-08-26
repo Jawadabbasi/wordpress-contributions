44f8bdbeebb38d1fa9aa537ebad7befaa9cf96ef
Jawadabbasi <Jawadabbasi42@gmail.com>
2022-08-26 23:02:39 +0500

    Add files via upload

---
+++ have_posts/README.md
@@ -0,0 +1,4 @@
+<b>Output:</b>
+Post title
+Post featured full size image (When anyone click on the image then open post single page)
+Post small description
---
+++ have_posts/index.php
@@ -0,0 +1,7 @@
+<?php if ( have_posts() ) : while( have_posts()  ) : the_post(); ?>
+    <h1><?php the_title(); ?></h1>
+    <a href="<?php the_permalink(); ?>">
+        <?php the_post_thumbnail( 'full' ); ?>
+    </a>
+    <p><?php the_excerpt(); ?></p>
+<?php endwhile; endif; ?>
