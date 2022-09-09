8bc395c9656ca694d8f04e6e61b6bb2477f60c3f
Jawad Abbasi <Jawadabbasi42@gmail.com>
2022-09-10 01:34:21 +0500

    Update index.php

--- how to show posts with pagination/index.php
+++ how to show posts with pagination/index.php
@@ -1,15 +1,13 @@
 <?php
-
 if(have_posts()):
-    while(have_posts()) : the_posts();
+   while(have_posts()) : the_posts();
 ?>
 <h1><?php the_title(); ?></h1>
 <div class="post-image"><?php the_post_thumbnail('medium'); ?></div>
 <php><?php the_excerpt(); ?></p>
 <?php
 endwhile;
-endif
-
+endif;
+    
 echo paginate_links();
-
 ?>
